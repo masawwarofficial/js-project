2ec087f09f6cebd926d6cee17f56929fbde2471d
masawwarofficial <137478303+masawwarofficial@users.noreply.github.com>
2023-09-18 18:06:50 +0000

    added 4 basics

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -2,6 +2,8 @@
 
 //prevent pollution of the global JS scope
 
+
+
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
@@ -10,3 +12,8 @@
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
+
+
+// if we need two iife function in a file then use ; after one function completed 
+
+//
